a503daf3f02323ff6dad919c1e6f27590dbc2ce1
hmirceb <hectorm94@gmail.com>
2026-03-05 12:00:59 +0100

    fixes

--- R/simulations.R
+++ R/simulations.R
@@ -99,56 +99,56 @@ sim_mvcomm <- function(n_sp = 10,
 #' The arguments in \code{syngenr} are set to predefined default values, which can of course
 #' be customised, or sampled from parameter spaces.
 #'
-#' @param years the length of the timeseries in years.
-#' @param n_sp number of species in the community.
-#' @param tot_abu total abundance of the community, representing e.g. the number of
+#' @param years Numeric. The length of the timeseries in years.
+#' @param n_sp Numeric. Number of species in the community.
+#' @param max_rel_abu Numeric. Maximum relative abundance for the species in the community.
+#' @param tot_abu Numeric. Total abundance of the community, representing e.g. the number of
 #' individuals or the total amount of biomass.
-#' @param power the slope of the relationship between log(mean) and log(variance) of
+#' @param power Numeric. The slope of the relationship between log(mean) and log(variance) of
 #' the abundances of the species.
-#' @param switch_env character vector of length 1, either "on" or "off". Defines if the
-#' species abundances repond to a hypothetical environmental cue.
-#' @param mean_env_resp the mean of the normal distribution from which each of the
+#' @param switch_env Boolean. Defines if the species abundances respond to a hypothetical environmental cue. Default FALSE.
+#' @param mean_env_resp Numeric. The mean of the normal distribution from which each of the
 #' species responses to the environemtnal cue is drawn.
-#' @param sd_env_resp the standard deviation around the mean of the normal distribution
+#' @param sd_env_resp Numeric. The standard deviation around the mean of the normal distribution
 #' from which each of the species responses to the environemtnal cue is drawn.
-#' @param bimodal_env Logical. Making the reponse to the environmental cue either
+#' @param bimodal_env Boolean. Making the reponse to the environmental cue either
 #' uniform among the species (if FALSE), or making the majority of half of the
-#' species respond positively, and the other half negatively.
-#' @param comp Logical. If TRUE, species exhibit compensatory dynamics, i.e. the gain
+#' species respond positively, and the other half negatively. Default FALSE.
+#' @param comp Boolean. If TRUE, species exhibit compensatory dynamics, i.e. the gain
 #' of abundance in a species from one year to the next, is compensated by the
 #' loss of abundance in another species, where the latter has a similar mean
-#' abundance value.
-#' @param switch_trend Either "on" or "off", defining if there is a general monotonic
-#' trend in abundances of species across the timeseries.
-#' @param mean_trend_resp the mean of the normal distribution from which each of the
+#' abundance value. Default FALSE.
+#' @param switch_trend Boolean. Defines if there is a general monotonic
+#' trend in abundances of species across the timeseries. Default FALSE.
+#' @param mean_trend_resp Numeric. The mean of the normal distribution from which each of the
 #' species responses to the longterm trend is drawn. Default is 1.
-#' @param sd_trend_resp the standard deviation around the mean of the normal
+#' @param sd_trend_resp Numeric. The standard deviation around the mean of the normal
 #' distribution from which each of the species responses to the longterm trend is
 #' drawn. Default is 1.
-#' @param bimodal_trend Logical. If TRUE, the majority of half of the species exhibit a
+#' @param bimodal_trend Boolean. If TRUE, the majority of half of the species exhibit a
 #' positive long term trend of abundances, and the other half expresses a
 #' negative long term trend. If FALSE, most of the species exhibit a uniform long
 #' term trend in abundances, depending on the value set for mean_trend and
-#' mean_sd.
-#' @param bound_pos Logical. If true, abundance values that are simulated to be
-#' negative, will be set to zero.
+#' mean_sd. Default FALSE.
+#' @param bound_pos Boolean. If TRUE, abundance values that are simulated to be
+#' negative, will be set to zero. Default TRUE.
 #'
-#' @return The output is a list with four elements
+#' @return A named list with four elements:
 #'
-#' time_species_matrix: the simulated temporal community data where species are
+#' time_species_matrix: The simulated temporal community data where species are
 #' columns and years are rows.
 #'
-#' param_years: values for the evnironmental cue and the trend throughout the
+#' param_years: Values for the environmental cue and the trend throughout the
 #' years. Note that these contain values even if the environment or trend are
 #' switched off.
 #'
-#' param_species: a data frame containing the responses to the environment and
+#' param_species: A data frame containing the responses to the environment and
 #' the long term trend, as well as the mean abundance and its standard deviation
 #' for each species in the community.
 #'
-#' param_general: a data frame with only one row, containing all the parameter
+#' param_general: A data frame with only one row, containing all the parameter
 #' settings from the function call.
-#' @export syngenr
+#' @export
 syngenr <- function(years = 100,
                     n_sp = 16,
                     max_rel_abu = 0.6,
@@ -227,7 +227,7 @@ syngenr <- function(years = 100,
   return(res)
 }
 
-#' Function to generate a geometric distribution of relative abundances
+#' Helper function to generate a geometric distribution of relative abundances
 #'
 #' @param max_rel_abu a single numeric value that represents the realtive
 #' abundance of the most abundant species in the community.
@@ -235,7 +235,7 @@ syngenr <- function(years = 100,
 #'
 #' @return A vector with the length of the number of species containing the
 #' relative abundances of the species.
-#' @export geom_seq
+#' @export
 geom_seq <- function(max_rel_abu, n_sp) {
   rel.abu <- max_rel_abu
   remaining <- 1 - rel.abu
@@ -248,18 +248,17 @@ geom_seq <- function(max_rel_abu, n_sp) {
 }
 
 
-#' Function to simulate response of species abundances to environmental cues
+#' Helper function to simulate response of species abundances to environmental cues
 #'
-#' \code{response} simulates the strength and direction with which the change in
+#' `response()` simulates the strength and direction with which the change in
 #' species abundance across time responds to a stochastic environmental cue, or,
 #' a general directional trend in abundance. This is only a helper function that
-#' is used internally in the main function to simulate temporal communities,
-#' syngenr.
+#' is used internally in `sim_mvcomm()` and `syngenr()`.
 #'
 #' @param state Boolean. Defines if the species have some kind of trend. Default TRUE.
 #' @param bimodal Boolean. If TRUE, half of the species respond opposite to the other
 #' half, if FALSE, all species respond in the same direction. How many species
-#' respond in a given direction also depends on the settings of mean and sd.
+#' respond in a given direction also depends on the settings of mean and sd. Default FALSE.
 #' @param mean Numeric. The mean of the normal distribution from which each of the species
 #' responses is drawn.
 #' @param sd Numeric. The standard deviation around the mean of the normal distribution from
@@ -268,13 +267,13 @@ geom_seq <- function(max_rel_abu, n_sp) {
 #' @param comp Boolean. If TRUE, species will exhibit compensatory dynamics. This is
 #' simulated by having species of similar abundance to respond in opposite
 #' directions. This argument is therefore only meaning full when having set
-#' bimodal = TRUE.
+#' bimodal = TRUE. Default FALSE.
 #'
 #' @return The output is a vector with the length of the number of species, each
 #' value representing the response of a species to hypotehtical cue (i.e. in the
 #' context of the simulation, either an environmental signal or a longterm
 #' monotonic abundance trend).
-#' @export response
+#' @export
 response <- function(state = TRUE,
                      bimodal = FALSE,
                      mean = 1,
